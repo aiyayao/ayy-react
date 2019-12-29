cf4269c6eadddd3e26e1c563c35af7c94d9af52c
renjie <rj.aiyayao@gmail.com>
2019-12-29 17:44:44 +0800

    refactor: optimized code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import Nav from 'components/Nav/Nav';
 import getRouter from 'router/router';
 
 class App extends Component {
+
+  getRouter = () => {
+    if (process.env.NODE_ENV === 'development') {
+      return HashRouter;
+    }
+    return BrowserRouter;
+  }
+
   render() {
+    const Router = this.getRouter();
     return (
-      <div id="app">
-        <Nav />
-        {getRouter()}
-      </div>
+      <Router>
+        <div id="app">
+          <Nav />
+          {getRouter()}
+        </div>
+      </Router>
     );
   }
 }
