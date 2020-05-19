f57bb509f363f4a983112f4129d4e29fddd73c07
renjie <rj.aiyayao@gmail.com>
2020-05-19 16:46:49 +0800

    feat: add single line text

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ class App extends Component {
         <div id="app">
           <Nav />
           {getRouter()}
+          'test commit4'
         </div>
       </Router>
     );
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from 'pages/Home/Home';
-import Page1 from 'pages/Page1/Page1';
-import Counter from 'pages/Counter/Counter';
-import UserInfo from 'pages/UserInfo/UserInfo';
-import NotFound from 'pages/NotFound/NotFound';
+import loadable from './loadable';
+const Home = loadable(()=>import('pages/Home/Home'));
+const Page1 = loadable(()=>import('pages/Page1/Page1'));
+const Counter = loadable(()=>import('pages/Counter/Counter'));
+const UserInfo = loadable(()=>import('pages/UserInfo/UserInfo'));
+const NotFound = loadable(()=>import('pages/NotFound/NotFound'));
+// import Home from 'pages/Home/Home';
+// import Page1 from 'pages/Page1/Page1';
+// import Counter from 'pages/Counter/Counter';
+// import UserInfo from 'pages/UserInfo/UserInfo';
+// import NotFound from 'pages/NotFound/NotFound';
 
 const getRouter = () => (
   <Switch>
